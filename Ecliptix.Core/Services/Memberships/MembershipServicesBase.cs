69b7f6cceff2eeb0209ec5ff22971f12ce05aeed
MrReptiloid <morgot1999@gmail.com>
2025-07-22 12:29:49 +0300

    refactor: simplify request handling in MembershipServices by introducing ExecuteWithDecryption method

--- Ecliptix.Core/Services/Memberships/MembershipServicesBase.cs
+++ Ecliptix.Core/Services/Memberships/MembershipServicesBase.cs
@@ -1,7 +1,12 @@
 using System.Globalization;
 using Akka.Actor;
 using Ecliptix.Core.Protocol;
+using Ecliptix.Core.Services.Utilities;
 using Ecliptix.Domain.Memberships.WorkerActors;
+using Ecliptix.Domain.Utilities;
+using Ecliptix.Protobuf.CipherPayload;
+using Google.Protobuf;
+using Grpc.Core;
 
 namespace Ecliptix.Core.Services.Memberships;
 
@@ -13,4 +18,29 @@ public abstract class MembershipServicesBase(
     protected readonly IActorRef MembershipActor = actorRegistry.Get<MembershipActor>();
     protected readonly ICipherPayloadHandler CipherPayloadHandler = cipherPayloadHandler;
     protected string CultureName { get; private set; } = CultureInfo.CurrentCulture.Name;
+    
+    
+    protected async Task<CipherPayload> ExecuteWithDecryption<TRequest, TResponse>(
+        CipherPayload encryptedRequest,
+        ServerCallContext context,
+        Func<TRequest, uint, CancellationToken, Task<CipherPayload>> businessLogic)
+        where TRequest : class, IMessage<TRequest>, new()
+        where TResponse : class, IMessage<TResponse>, new()
+    {
+        uint connectId = ServiceUtilities.ExtractConnectId(context);
+
+
+        Result<byte[], FailureBase> decryptionResult = await CipherPayloadHandler.DecryptRequest(encryptedRequest, connectId, context);
+
+        if (decryptionResult.IsErr)
+        {
+            return await CipherPayloadHandler.RespondFailure<TResponse>(
+                decryptionResult.UnwrapErr(), connectId, context);
+        }
+
+        byte[] decryptedBytes = decryptionResult.Unwrap();
+        TRequest parsedRequest = Helpers.ParseFromBytes<TRequest>(decryptedBytes);
+
+        return await businessLogic(parsedRequest, connectId, context.CancellationToken);
+    }
 }
