790c3540634dc2fd4b236aff0082ca43bb6a8673
MrReptiloid <morgot1999@gmail.com>
2025-07-22 16:59:31 +0300

    refactor: enhance AppDeviceServiceBase and AppDeviceServices with improved request handling and encryption methods

--- Ecliptix.Core/Services/AppDeviceServiceBase.cs
+++ Ecliptix.Core/Services/AppDeviceServiceBase.cs
@@ -1,15 +1,79 @@
 using Akka.Actor;
 using Akka.Hosting;
 using Ecliptix.Core.Protocol.Actors;
+using Ecliptix.Core.Services.Utilities;
+using Ecliptix.Core.Services.Utilities.CipherPayloadHandler;
 using Ecliptix.Domain.AppDevices.Persistors;
+using Ecliptix.Domain.Utilities;
 using Ecliptix.Protobuf.AppDeviceServices;
+using Ecliptix.Protobuf.CipherPayload;
+using Ecliptix.Protobuf.PubKeyExchange;
+using Google.Protobuf;
 using Grpc.Core;
 
 namespace Ecliptix.Core.Services;
 
-public abstract class AppDeviceServiceBase(IEcliptixActorRegistry actorRegistry)
+public abstract class AppDeviceServiceBase(
+    IEcliptixActorRegistry actorRegistry,
+    ICipherPayloadHandlerFactory cipherPayloadHandlerFactory)
     : AppDeviceServiceActions.AppDeviceServiceActionsBase
 {
     protected readonly IActorRef AppDevicePersistorActor = actorRegistry.Get<AppDevicePersistorActor>();
     protected readonly IActorRef ProtocolActor = actorRegistry.Get<EcliptixProtocolSystemActor>();
+    protected readonly ICipherPayloadHandler CipherPayloadHandler =
+        cipherPayloadHandlerFactory.Create<EcliptixProtocolSystemActor>();
+    
+    protected async Task<TResponse> ExecutePlain<TRequest, TResponse>(
+        TRequest request,
+        ServerCallContext context,
+        Func<TRequest, uint, CancellationToken, Task<Result<TResponse, FailureBase>>> handler)
+        where TRequest : class
+        where TResponse : class
+    {
+        uint connectId = ServiceUtilities.ExtractConnectId(context);
+        var result = await handler(request, connectId, context.CancellationToken);
+
+        if (result.IsOk) return result.Unwrap();
+        throw GrpcFailureException.FromDomainFailure(result.UnwrapErr());
+    }
+
+    protected async Task<CipherPayload> ExecuteEncrypted<TRequest, TResponse>(
+        CipherPayload encryptedRequest,
+        ServerCallContext context,
+        PubKeyExchangeType exchangeType,
+        Func<TRequest, uint, CancellationToken, Task<Result<TResponse, FailureBase>>> handler)
+        where TRequest : class, IMessage<TRequest>, new()
+        where TResponse : class, IMessage<TResponse>, new()
+    {
+        uint connectId = ServiceUtilities.ExtractConnectId(context);
+
+        DecryptCipherPayloadActorEvent
+            decryptEvent = new DecryptCipherPayloadActorEvent(exchangeType, encryptedRequest);
+        ForwardToConnectActorEvent decryptForwarder = new ForwardToConnectActorEvent(connectId, decryptEvent);
+        Result<byte[], EcliptixProtocolFailure> decryptResult =
+            await ProtocolActor.Ask<Result<byte[], EcliptixProtocolFailure>>(decryptForwarder,
+                context.CancellationToken);
+
+        if (decryptResult.IsErr)
+            return await CipherPayloadHandler.RespondFailure<TResponse>(decryptResult.UnwrapErr(), connectId, context);
+
+        TRequest parsedRequest = Helpers.ParseFromBytes<TRequest>(decryptResult.Unwrap());
+
+        Result<TResponse, FailureBase> handlerResult =
+            await handler(parsedRequest, connectId, context.CancellationToken);
+        if (handlerResult.IsErr)
+            return await CipherPayloadHandler.RespondFailure<TResponse>(handlerResult.UnwrapErr(), connectId, context);
+
+        EncryptPayloadActorEvent encryptCommand =
+            new EncryptPayloadActorEvent(exchangeType, handlerResult.Unwrap().ToByteArray());
+        
+        ForwardToConnectActorEvent encryptForwarder = new ForwardToConnectActorEvent(connectId, encryptCommand);
+        Result<CipherPayload, EcliptixProtocolFailure> encryptResult =
+            await ProtocolActor.Ask<Result<CipherPayload, EcliptixProtocolFailure>>(encryptForwarder,
+                context.CancellationToken);
+
+        return encryptResult.IsOk
+            ? encryptResult.Unwrap()
+            : await CipherPayloadHandler.RespondFailure<TResponse>(encryptResult.UnwrapErr(), connectId, context);
+    }
 }
--- Ecliptix.Core/Services/AppDeviceServices.cs
+++ Ecliptix.Core/Services/AppDeviceServices.cs
@@ -1,120 +1,88 @@
 using Akka.Actor;
-using Akka.Hosting;
 using Ecliptix.Core.Protocol.Actors;
-using Ecliptix.Core.Services.Utilities;
+using Ecliptix.Core.Services.Utilities.CipherPayloadHandler;
 using Ecliptix.Domain.AppDevices.Events;
 using Ecliptix.Domain.AppDevices.Failures;
 using Ecliptix.Domain.Utilities;
 using Ecliptix.Protobuf.AppDevice;
 using Ecliptix.Protobuf.CipherPayload;
 using Ecliptix.Protobuf.PubKeyExchange;
-using Google.Protobuf;
 using Grpc.Core;
 
 namespace Ecliptix.Core.Services;
 
-public class AppDeviceServices(IEcliptixActorRegistry actorRegistry)
-    : AppDeviceServiceBase(actorRegistry)
+public class AppDeviceServices(
+    IEcliptixActorRegistry actorRegistry,
+    ICipherPayloadHandlerFactory cipherPayloadHandlerFactory)
+    : AppDeviceServiceBase(actorRegistry, cipherPayloadHandlerFactory)
 {
     public override async Task<RestoreSecrecyChannelResponse> RestoreAppDeviceSecrecyChannel(
         RestoreSecrecyChannelRequest request, ServerCallContext context)
     {
-        uint connectId = ServiceUtilities.ExtractConnectId(context);
-
-        ForwardToConnectActorEvent forwarderEvent = new(connectId, new RestoreAppDeviceSecrecyChannelState());
-        Result<RestoreSecrecyChannelResponse, EcliptixProtocolFailure> syncStateResult =
-            await ProtocolActor.Ask<Result<RestoreSecrecyChannelResponse, EcliptixProtocolFailure>>(
-                forwarderEvent,
-                context.CancellationToken);
-
-        if (syncStateResult.IsErr)
-        {
-            EcliptixProtocolFailure ecliptixProtocolFailure = syncStateResult.UnwrapErr();
-
-            if (ecliptixProtocolFailure.FailureType == EcliptixProtocolFailureType.ActorRefNotFound)
+        return await ExecutePlain<RestoreSecrecyChannelRequest, RestoreSecrecyChannelResponse>(
+            request,
+            context,
+            async (_, connectId, ct) =>
             {
-                return new RestoreSecrecyChannelResponse
+                ForwardToConnectActorEvent forwardEvent = new ForwardToConnectActorEvent(connectId, new RestoreAppDeviceSecrecyChannelState());
+                Result<RestoreSecrecyChannelResponse, EcliptixProtocolFailure> restoreResult =
+                    await ProtocolActor
+                        .Ask<Result<RestoreSecrecyChannelResponse, EcliptixProtocolFailure>>(forwardEvent, ct);
+
+                if (restoreResult.IsErr)
                 {
-                    Status = RestoreSecrecyChannelResponse.Types.RestoreStatus.SessionNotFound
-                };
-            }
+                    var failure = restoreResult.UnwrapErr();
+                    if (failure.FailureType == EcliptixProtocolFailureType.ActorRefNotFound || ProtocolActor.IsNobody())
+                    {
+                        return Result<RestoreSecrecyChannelResponse, FailureBase>.Ok(new RestoreSecrecyChannelResponse
+                        {
+                            Status = RestoreSecrecyChannelResponse.Types.RestoreStatus.SessionNotFound
+                        });
+                    }
 
-            throw GrpcFailureException.FromDomainFailure(syncStateResult.UnwrapErr());
-        }
+                    return Result<RestoreSecrecyChannelResponse, FailureBase>.Err(failure);
+                }
 
-        if (ProtocolActor.IsNobody())
-        {
-            return new RestoreSecrecyChannelResponse
-            {
-                Status = RestoreSecrecyChannelResponse.Types.RestoreStatus.SessionNotFound
-            };
-        }
-
-        RestoreAppDeviceSecrecyChannelState restoreAppDeviceSecrecyChannelState = new();
-        ForwardToConnectActorEvent forwarder = new(connectId, restoreAppDeviceSecrecyChannelState);
-
-        syncStateResult =
-            await ProtocolActor.Ask<Result<RestoreSecrecyChannelResponse, EcliptixProtocolFailure>>(forwarder,
-                context.CancellationToken);
-
-        if (syncStateResult.IsOk) return syncStateResult.Unwrap();
-        throw GrpcFailureException.FromDomainFailure(syncStateResult.UnwrapErr());
+                return Result<RestoreSecrecyChannelResponse, FailureBase>.Ok(restoreResult.Unwrap());
+            });
     }
 
-    public override async Task<PubKeyExchange> EstablishAppDeviceSecrecyChannel(PubKeyExchange request,
-        ServerCallContext context)
+    public override async Task<PubKeyExchange> EstablishAppDeviceSecrecyChannel(
+        PubKeyExchange request, ServerCallContext context)
     {
-        uint connectId = ServiceUtilities.ExtractConnectId(context);
+        return await ExecutePlain<PubKeyExchange, PubKeyExchange>(
+            request,
+            context,
+            async (parsedRequest, connectId, ct) =>
+            {
+                BeginAppDeviceEphemeralConnectActorEvent actorEvent =
+                    new BeginAppDeviceEphemeralConnectActorEvent(parsedRequest, connectId);
+                
+                Result<DeriveSharedSecretReply, EcliptixProtocolFailure> reply =
+                    await ProtocolActor.Ask<Result<DeriveSharedSecretReply, EcliptixProtocolFailure>>(actorEvent, ct);
 
-        BeginAppDeviceEphemeralConnectActorEvent actorEvent = new(request, connectId);
-
-        Result<DeriveSharedSecretReply, EcliptixProtocolFailure> deriveSharedSecretReply =
-            await ProtocolActor.Ask<Result<DeriveSharedSecretReply, EcliptixProtocolFailure>>(
-                actorEvent,
-                context.CancellationToken);
-
-        if (deriveSharedSecretReply.IsOk) return deriveSharedSecretReply.Unwrap().PubKeyExchange;
-        throw GrpcFailureException.FromDomainFailure(deriveSharedSecretReply.UnwrapErr());
+                return reply.IsOk
+                    ? Result<PubKeyExchange, FailureBase>.Ok(reply.Unwrap().PubKeyExchange)
+                    : Result<PubKeyExchange, FailureBase>.Err(reply.UnwrapErr());
+            });
     }
+
 
     public override async Task<CipherPayload> RegisterDeviceAppIfNotExist(
-        CipherPayload request,
-        ServerCallContext context)
+        CipherPayload request, ServerCallContext context)
     {
-        uint connectId = ServiceUtilities.ExtractConnectId(context);
+        return await ExecuteEncrypted<AppDevice, AppDeviceRegisteredStateReply>(
+            request,
+            context,
+            PubKeyExchangeType.DataCenterEphemeralConnect,
+            async (appDevice, _ ,ct) =>
+            {
+                var registerResult = await AppDevicePersistorActor.Ask<Result<AppDeviceRegisteredStateReply, AppDeviceFailure>>(
+                    new RegisterAppDeviceIfNotExistActorEvent(appDevice), ct);
 
-        DecryptCipherPayloadActorEvent decryptEvent = new(PubKeyExchangeType.DataCenterEphemeralConnect,
-            request
-        );
-
-        ForwardToConnectActorEvent decryptForwarder = new(connectId, decryptEvent);
-
-        Result<byte[], EcliptixProtocolFailure> decryptResult = await ProtocolActor
-            .Ask<Result<byte[], EcliptixProtocolFailure>>(decryptForwarder, context.CancellationToken);
-
-        if (decryptResult.IsErr) throw GrpcFailureException.FromDomainFailure(decryptResult.UnwrapErr());
-
-        AppDevice appDevice = Helpers.ParseFromBytes<AppDevice>(decryptResult.Unwrap());
-        Result<AppDeviceRegisteredStateReply, AppDeviceFailure> persistorResult = await AppDevicePersistorActor
-            .Ask<Result<AppDeviceRegisteredStateReply, AppDeviceFailure>>(
-                new RegisterAppDeviceIfNotExistActorEvent(appDevice),
-                context.CancellationToken
-            );
-
-        if (persistorResult.IsErr) throw GrpcFailureException.FromDomainFailure(persistorResult.UnwrapErr());
-
-        AppDeviceRegisteredStateReply reply = persistorResult.Unwrap();
-
-        EncryptPayloadActorEvent encryptCommand = new(PubKeyExchangeType.DataCenterEphemeralConnect,
-            reply.ToByteArray()
-        );
-
-        ForwardToConnectActorEvent encryptForwarder = new(connectId, encryptCommand);
-        Result<CipherPayload, EcliptixProtocolFailure> encryptResult = await ProtocolActor
-            .Ask<Result<CipherPayload, EcliptixProtocolFailure>>(encryptForwarder, context.CancellationToken);
-
-        if (encryptResult.IsOk) return encryptResult.Unwrap();
-
-        throw GrpcFailureException.FromDomainFailure(encryptResult.UnwrapErr());
+                return registerResult.IsOk
+                    ? Result<AppDeviceRegisteredStateReply, FailureBase>.Ok(registerResult.Unwrap())
+                    : Result<AppDeviceRegisteredStateReply, FailureBase>.Err(registerResult.UnwrapErr());
+            });
     }
 }
