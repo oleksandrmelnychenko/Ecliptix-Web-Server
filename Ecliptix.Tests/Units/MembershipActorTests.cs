eefe6e33661931e52e9ad6a3dc99951e7d7259c5
MrReptiloid <morgot1999@gmail.com>
2025-08-04 22:44:12 +0300

    test: enhance MembershipActor tests to cover all sign-in scenarios

--- Ecliptix.Tests/Units/MembershipActorTests.cs
+++ Ecliptix.Tests/Units/MembershipActorTests.cs
@@ -20,54 +20,95 @@ public sealed class MembershipActorTests : TestKit
     private TestProbe _persistorProbe;
     private IActorRef _membershipActor;
     
-    private readonly SignInMembershipActorEvent signInEvent = new
-    (
-        PhoneNumber: "+380500000000",
-        OpaqueSignInInitRequest: new OpaqueSignInInitRequest(),
-        CultureName: "uk-UA"
-    );
-
     [TestInitialize]
-    public void Initialize()
+    public void Setup()
     {
-        _opaqueMock = new Mock<IOpaqueProtocolService>();
         _persistorProbe = CreateTestProbe();
+        _opaqueMock = new Mock<IOpaqueProtocolService>();
 
-        Mock<ILocalizationProvider> localizationMock = new();
-        localizationMock.Setup(x => x.Localize(It.IsAny<string>(), It.IsAny<string>())).Returns("msg");
-        
+        var localizationMock = new Mock<ILocalizationProvider>();
+        localizationMock.Setup(x => x.Localize("InvalidCredentials", It.IsAny<string>())).Returns("invalid");
+        localizationMock.Setup(x => x.Localize("TooManySigninAttempts", It.IsAny<string>())).Returns("rate-limit");
+
         _membershipActor = Sys.ActorOf(MembershipActor.Build(_persistorProbe.Ref, _opaqueMock.Object, localizationMock.Object));
     }
-    
+
+
     [TestMethod]
-    public async Task HandleSignInMembership_ReturnSuccess_WhenPersistorAndOpaqueSuccess()
+    [DataRow(null, false, OpaqueSignInInitResponse.Types.SignInResult.Succeeded, DisplayName = "Returns_Ok_When_All_Succeed")]
+    [DataRow(null, true, null, DisplayName = "Returns_InvalidOpaque_When_InitiateSignIn_Fails")]
+    [DataRow(VerificationFlowFailureType.Validation, false, OpaqueSignInInitResponse.Types.SignInResult.InvalidCredentials, DisplayName = "Returns_TranslatedFailure_When_Persistor_Fails_Validation")]
+    [DataRow(VerificationFlowFailureType.RateLimitExceeded, false, OpaqueSignInInitResponse.Types.SignInResult.LoginAttemptExceeded, DisplayName = "Returns_TranslatedFailure_When_Persistor_Fails_RateLimit")]
+    [DataRow(VerificationFlowFailureType.Generic, false, null, DisplayName = "Returns_Err_When_Persistor_Fails_Unexpectedly")]
+    public void HandleSignInMembership_Handles_AllCases(object? failureTypeObj, bool opaqueShouldFail, object? expectedResultObj)
+
     {
-        MembershipQueryRecord membershipRecord = new()
+         // Arrange
+         SignInMembershipActorEvent @event = new
+        (
+            PhoneNumber: "+380500000000",
+            OpaqueSignInInitRequest: new OpaqueSignInInitRequest(),
+            CultureName: "uk-UA"
+        );
+
+        if (failureTypeObj is null)
         {
-            SecureKey = [1, 2, 3, 4],
-            UniqueIdentifier = Guid.NewGuid(),
-            ActivityStatus = Membership.Types.ActivityStatus.Active,
-            CreationStatus = Membership.Types.CreationStatus.SecureKeySet
-        };
+            // Success persistor
+            var membershipRecord = new MembershipQueryRecord
+            {
+                SecureKey = new byte[] { 1, 2, 3 },
+                UniqueIdentifier = Guid.NewGuid(),
+                ActivityStatus = Membership.Types.ActivityStatus.Active,
+                CreationStatus = Membership.Types.CreationStatus.PassphraseSet
+            };
 
-        OpaqueSignInInitResponse opaqueResponse = new()
+            Result<OpaqueSignInInitResponse, OpaqueFailure> opaqueResult = opaqueShouldFail
+                ? Result<OpaqueSignInInitResponse, OpaqueFailure>.Err(OpaqueFailure.InvalidInput("failed"))
+                : Result<OpaqueSignInInitResponse, OpaqueFailure>.Ok(new OpaqueSignInInitResponse
+                {
+                    Result = OpaqueSignInInitResponse.Types.SignInResult.Succeeded,
+                    Message = "ok"
+                });
+
+            _opaqueMock
+                .Setup(x => x.InitiateSignIn(It.IsAny<OpaqueSignInInitRequest>(), It.IsAny<MembershipOpaqueQueryRecord>()))
+                .Returns(opaqueResult);
+
+            _membershipActor.Tell(@event, TestActor);
+
+            _persistorProbe.ExpectMsg<SignInMembershipActorEvent>();
+            _persistorProbe.Sender.Tell(Result<MembershipQueryRecord, VerificationFlowFailure>.Ok(membershipRecord));
+        }
+        else
         {
-            Result = OpaqueSignInInitResponse.Types.SignInResult.Succeeded
-        };
-        
-        _opaqueMock
-            .Setup(x => x.InitiateSignIn(It.IsAny<OpaqueSignInInitRequest>(), It.IsAny<MembershipOpaqueQueryRecord>()))
-            .Returns(Result<OpaqueSignInInitResponse, OpaqueFailure>.Ok(opaqueResponse));
+            // Failure persistor
+            Console.WriteLine("1");
+            VerificationFlowFailureType failureType = (VerificationFlowFailureType)failureTypeObj;
+            Console.WriteLine("2");
+            VerificationFlowFailure failure = new VerificationFlowFailure(failureType, "msg");
 
-        // Act
-        _membershipActor.Tell(signInEvent, TestActor);
-
-        _persistorProbe.ExpectMsg<SignInMembershipActorEvent>();
-        _persistorProbe.Sender.Tell(Result<MembershipQueryRecord, VerificationFlowFailure>.Ok(membershipRecord));
+            Console.WriteLine("3");
+            _membershipActor.Tell(@event, TestActor);
+            Console.WriteLine("4");
+            _persistorProbe.ExpectMsg<SignInMembershipActorEvent>();
+            Console.WriteLine("5");
+            _persistorProbe.Sender.Tell(Result<MembershipQueryRecord, VerificationFlowFailure>.Err(failure));
+            Console.WriteLine("6");
+        }
 
         // Assert
-        var result = ExpectMsg<Result<OpaqueSignInInitResponse, VerificationFlowFailure>>();
-        Assert.IsTrue(result.IsOk);
-        Assert.AreEqual(OpaqueSignInInitResponse.Types.SignInResult.Succeeded, result.Unwrap().Result);
+        Console.WriteLine("7");
+        Result<OpaqueSignInInitResponse, VerificationFlowFailure> result =
+            ExpectMsg<Result<OpaqueSignInInitResponse, VerificationFlowFailure>>();
+
+        if (expectedResultObj is null)
+        {
+            Assert.IsTrue(result.IsErr);
+        }
+        else
+        {
+            Assert.IsTrue(result.IsOk);
+            Assert.AreEqual((OpaqueSignInInitResponse.Types.SignInResult)expectedResultObj, result.Unwrap().Result);
+        }
     }
 }
