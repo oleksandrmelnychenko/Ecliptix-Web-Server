aadf8359e82545efe5cbbbbfcb315af9f425e766
MrReptiloid <morgot1999@gmail.com>
2025-09-03 19:07:22 +0300

    feat: add local provider and enhance EC2 instance outputs for better resource management

--- Scripts/Terraform/ec2.tf
+++ Scripts/Terraform/ec2.tf
@@ -1,7 +1,7 @@
-# --- Два EC2 інстанси для ecliptix-control ---
+# ---  EC2 інстанс для ecliptix-control ---
 
 resource "aws_instance" "ecliptix_control" {
-  ami             = "ami-02003f9f0fde924ea" # Ubuntu 24.04 LTS 
+  ami             = "ami-02003f9f0fde924ea"  
   instance_type   = "t3.medium"
   subnet_id       = aws_subnet.ecliptix_public[0].id
   key_name        = aws_key_pair.ecliptix_key.key_name
--- Scripts/Terraform/outputs.tf
+++ Scripts/Terraform/outputs.tf
@@ -1,5 +1,9 @@
 output "ecliptix_control_public_ips" {
-  value = aws_instance.ecliptix_control[*].public_ip
+  value = aws_instance.ecliptix_control.public_ip
+}
+
+output "ecliptix_control_user" {
+  value = "ubuntu"
 }
 
 output "ecliptix_private_key_ed25519" {
@@ -9,4 +13,27 @@ output "ecliptix_private_key_ed25519" {
 
 output "alb_dns" {
   value = aws_lb.ecliptix.dns_name
+}
+
+
+output "ecr_repo" {
+  value = aws_ecr_repository.memberships.repository_url
+}
+
+output "ecs_cluster" {
+  value = aws_ecs_cluster.ecliptix.name
+}
+
+output "ecs_memberships_service" {
+  value = aws_ecs_service.memberships.name
+} 
+
+# --- Files ---
+
+resource "local_file" "ansible_invertory" {
+  content = <<EOT
+[jenkins]
+${aws_instance.ecliptix_control.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~./ssh/ecliptix-control-key.pem
+EOT
+  filename = "${path.module}/inventory.ini"
 }
