9f51ee9ee5545f043ad7f37b0d186b242258b427
MrReptiloid <morgot1999@gmail.com>
2025-09-05 17:02:13 +0300

    feat: update Jenkins deployment playbook to include Nginx configuration and enhance plugin installation

--- Scripts/deploy_infrastructure.sh
+++ Scripts/deploy_infrastructure.sh
@@ -4,4 +4,4 @@ cd Terraform
 terraform apply -auto-approve
 
 cd ../
-ansible-playbook -i Ansible/inventory.ini Ansible/jenkins.yml
+ansible-playbook -i Ansible/inventory.ini Ansible/jenkins.yml Ansible/nginx.yml
