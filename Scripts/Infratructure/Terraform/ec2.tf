499d66d18999b1f520bc27cad674de2b0d79d3d5
MrReptiloid <morgot1999@gmail.com>
2025-09-08 19:16:42 +0300

    feat: add Ansible playbooks for dependency installation and Docker setup, and create Jenkins pipeline job

--- Scripts/Infratructure/Terraform/ec2.tf
+++ Scripts/Infratructure/Terraform/ec2.tf
@@ -6,5 +6,12 @@ resource "aws_instance" "ecliptix_control" {
   subnet_id       = aws_subnet.ecliptix_public[0].id
   key_name        = aws_key_pair.ecliptix_key.key_name
   vpc_security_group_ids = [aws_security_group.ecliptix_control_sg.id]
+  
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+  
   tags = { Name = "ecliptix-control" }
 }
--- Scripts/Infratructure/deploy_infrastructure.sh
+++ Scripts/Infratructure/deploy_infrastructure.sh
@@ -6,4 +6,4 @@ cd Terraform
 terraform apply -auto-approve | tee "../Logs/${current_datetime}-log.txt"
 
 cd ../
-ansible-playbook -i Ansible/inventory.ini Ansible/main.yml | tee -a "Logs/${current_datetime}-log.txt"
+ansible-playbook -i Ansible/inventory.ini Ansible/main.yml -v | tee -a "Logs/${current_datetime}-log.txt"
