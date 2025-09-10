7aaddb5631f8239da96af0c45ec61cdac78f49f5
MrReptiloid <morgot1999@gmail.com>
2025-09-10 11:36:36 +0300

    feat: configure ECS task IAM roles and policies for CloudWatch logging

--- Scripts/Infratructure/Terraform/cloudwatch.tf
+++ Scripts/Infratructure/Terraform/cloudwatch.tf
@@ -1,4 +1,1 @@
-resource "aws_cloudwatch_log_group" "memberships" {
-  name              = "/ecs/ecliptix-memberships"
-  retention_in_days = 30
-}
+
--- Scripts/Infratructure/Terraform/ecs_task.tf
+++ Scripts/Infratructure/Terraform/ecs_task.tf
@@ -17,6 +17,10 @@ resource "aws_ecs_task_definition" "memberships" {
         { containerPort = 5051, protocol = "tcp" },
         { containerPort = 8080, protocol = "tcp" }
       ]
+
+      execution_role_arn = aws_iam_role.ecs_task_role.arn
+      task_role_arn      = aws_iam_role.ecs_task_role.arn
+
       environment = [
         { 
           name = "DOTNET_ENVIRONMENT",
@@ -27,14 +31,6 @@ resource "aws_ecs_task_definition" "memberships" {
           value = "Server=${aws_db_instance.memberships_mssql.address};Database=memberships;User Id=${jsondecode(data.aws_secretsmanager_secret_version.memberships_mssql.secret_string)["username"]};Password=${jsondecode(data.aws_secretsmanager_secret_version.memberships_mssql.secret_string)["password"]};Encrypt=True;TrustServerCertificate=True;"
         }
       ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-group"         = "/ecs/ecliptix-memberships"
-          "awslogs-region"        = "eu-central-1"
-          "awslogs-stream-prefix" = "ecs"
-        }
-      }
     }
   ])
 }
