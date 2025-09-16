b925a74cb7277bb4203e28f5ccd64862dd85aca2
MrReptiloid <morgot1999@gmail.com>
2025-09-16 16:18:19 +0300

    remove: old terraform files

--- Scripts/Infratructure/Terraform/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "memberships" {
-  name              = "ecliptix-memberships-logs"
-  retention_in_days = 30
-}
--- Scripts/Infratructure/Terraform/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "memberships" {
-  name                 = "ecliptix/memberships"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
