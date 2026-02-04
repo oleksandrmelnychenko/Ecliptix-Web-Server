e4e844734d670fc2deb97cfca5f9618c65593ea0
Vitaliy <vitalik.koliesnikov@gmail.com>
2026-02-04 12:11:38 +0200

    Migrations added

---
+++ src/Contexts/IdentityAccess/Infrastructure/Migrations/20260202143616_AddProfileSetStatus.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecliptix.IdentityAccess.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileSetStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CHK_Memberships_CreationStatus",
+                table: "memberships");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CHK_Memberships_CreationStatus",
+                table: "memberships",
+                sql: "creation_status IN ('otp_verified', 'secure_key_set', 'profile_set', 'passphrase_set')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CHK_Memberships_CreationStatus",
+                table: "memberships");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CHK_Memberships_CreationStatus",
+                table: "memberships",
+                sql: "creation_status IN ('otp_verified', 'secure_key_set', 'passphrase_set')");
+        }
+    }
+}
